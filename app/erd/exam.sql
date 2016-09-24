79dea44956aafa1f95d1b7673413a2407a9959de
Ismelito Malonzo <idmlnz@yahoo.com>
2016-09-23 19:22:43 -0700

    Preliminary files for exam

---
+++ app/erd/exam.sql
@@ -0,0 +1,34 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema examdb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema examdb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `examdb` DEFAULT CHARACTER SET utf8 ;
+USE `examdb` ;
+
+-- -----------------------------------------------------
+-- Table `examdb`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `examdb`.`user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `firstname` VARCHAR(45) NOT NULL,
+  `lastname` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
---
+++ app/models/Register.py
@@ -0,0 +1,95 @@
+from flask.ext.bcrypt import Bcrypt
+from system.core.model import Model
+import re
+
+class Register(Model):
+    def __init__(self):
+        super(Register, self).__init__()
+
+    def checkUser(self, info):
+        EMAIL_REGEX = re.compile(r'^[a-za-z0-9\.\+_-]+@[a-za-z0-9\._-]+\.[a-za-z]*$')
+        errors = []
+
+        if not info['email']:
+            errors.append('Email cannot be blank')
+        elif not EMAIL_REGEX.match(info['email']):
+            errors.append('Email format must be valid!')
+        if not info['password']:
+            errors.append('Password cannot be blank')
+        elif len(info['password']) < 1:
+            errors.append('Password must be at least 8 characters long')
+
+        if errors:
+            return {"status": False, "errors": errors}
+        else:
+            get_user_query = "SELECT * FROM user WHERE email=\"{}\"".format(info['email'])
+            user = self.db.query_db(get_user_query)[0]
+            if (info['password'] == user['password']):
+                return {"status": True, "user": user}
+            else:
+                errors.append('Your email does not match your password!!')
+                return {"status": False, "errors": errors}
+
+    def createUser(self, info):
+        EMAIL_REGEX = re.compile(r'^[a-za-z0-9\.\+_-]+@[a-za-z0-9\._-]+\.[a-za-z]*$')
+        errors = []
+        # Some basic validation
+        if not info['firstname']:
+            errors.append('firstname cannot be blank')
+        elif len(info['firstname']) < 2:
+            errors.append('firstname must be at least 2 characters long')
+
+        if not info['lastname']:
+            errors.append('lastname cannot be blank')
+        elif len(info['lastname']) < 2:
+            errors.append('lastname must be at least 2 characters long')
+
+        if not info['email']:
+            errors.append('Email cannot be blank')
+        elif not EMAIL_REGEX.match(info['email']):
+            errors.append('Email format must be valid [myemail@yahoo.com] !')
+
+        if not info['password']:
+            errors.append('Password cannot be blank')
+        elif len(info['password']) < 1:
+            errors.append('Password must be at least 8 characters long')
+
+        elif info['password'] != info['confirm_password']:
+            errors.append('Password and confirmation must match!')
+
+        if errors:
+            return {"status": False, "errors": errors}
+
+        else:
+            # add user to DB
+            #pw_hash = bcrypt.generate_password_hash(info['password'])
+            pw_hash = info['password']
+
+            #check if user already exists
+            get_user_query = "SELECT * FROM user where email=\"{}\"".format(info['email'])
+            print "query: {}".format(get_user_query)
+            try:
+                user = self.db.query_db(get_user_query)
+                print "user: {}".format(user)
+                if user:
+                    errors.append("{} already exists! Try login-in".format(info['email']))
+                    return {"status": False, "errors": errors}
+            except:
+              pass
+
+            insertQuery = "INSERT INTO user (firstname, lastname, email, password, created_at, updated_at) \
+                VALUES (:firstname, :lastname, :email, :password, NOW(), NOW())"
+
+            userData = {
+                'firstname': info['firstname'],
+                'lastname': info['lastname'],
+                'email': info['email'],
+                'password': pw_hash
+            }
+            print "userdata: {}".format(userData)
+            self.db.query_db(insertQuery, userData)
+
+            get_user_query = "SELECT * FROM user ORDER BY id DESC LIMIT 1"
+            user = self.db.query_db(get_user_query)
+            return {"status": True, "user": user[0]}
+
